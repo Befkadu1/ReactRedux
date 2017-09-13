7fa938372021d380e9b2d4b6fce33152db3505b2
Befkadu Degefa <befkassefa@gmail.com>
2017-09-13 17:15:54 +0200

    implementing reducers

---
+++ src/containers/book-detail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux' // single property if writen like this { connect }
+
+class BookDetail extends Component {
+
+    render() {
+        if(!this.props.book) {
+
+            return <div>Select a book to get Started</div>
+        }
+        return(
+            <div>
+                <h4>Details for: </h4>
+                <div>Title: {this.props.book.title}</div>
+                <div>Pages: {this.props.book.pages}</div>
+            </div>
+        );
+    }
+
+}
+
+function mapStateToProps (state) {
+    // Whatever is returned will show up as props inside of BookList container
+    return {
+        book: state.activeBook,
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail)
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'JavaScript: The Good Parts', pages: 100},
+        {title: 'Harry Potter', pages: 120},
+        {title: 'The dark Tower', pages: 10},
+        {title: 'Java', pages: 200},
+    ]
+}
