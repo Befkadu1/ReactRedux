8064b22e02fe1bc3d1b50917e24dfd438d330820
Befkadu Degefa <befkassefa@gmail.com>
2017-10-04 17:23:13 +0200

    Adding a comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 export function selectBook(book) {
 
     //selectBook is an ActionCreater, it needs to return an action,
-    // an object with a type property.
+    // an object with a type property. Action sent to all reducers
     return {
         type: 'BOOK_SELECTED',
         payload: book,
